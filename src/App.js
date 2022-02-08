48f69a23c705503e2cd3554184e651d4506ac2f8
andreavdiaz <andreavdiaz1994@gmail.com>
2022-02-08 12:26:04 -0500

    todomylist

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import Container from "./components/container";
+
+function App() {
+  return (
+  <div className="App">
+  <Container />
+      </div>);
+}
+
+export default App;
+
